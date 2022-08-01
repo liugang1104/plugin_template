0ede7119d8f56fe5bfa091bb5d024b2f8c3ff64b
mark.liu <mark.liu@ambergroup.io>
2022-08-01 16:56:45 +0800

    fix: update script

--- __brick__/{{project}}/{{project}}_platform_interface/gen_code.sh
+++ __brick__/{{project}}/{{project}}_platform_interface/gen_code.sh
@@ -2,20 +2,21 @@
 
 #1.新建DSL工作区
 cd ../
-PLUGIN_DIR=`pwd`
-echo $PLUGIN_DIR
+PLUGIN_DIR=$(pwd)
+echo "$PLUGIN_DIR"
 
-PLUGIN_NAME=`basename $(pwd)`
-echo $PLUGIN_NAME
+PLUGIN_NAME=$(basename $(pwd))
+echo "$PLUGIN_NAME"
 
-WORKSPACE_DIR="$PLUGIN_DIR/.workspace"
-mkdir $WORKSPACE_DIR
+WORKSPACE_DIR="$PLUGIN_DIR/workspace"
+mkdir "$WORKSPACE_DIR"
 
 #2.将dsl复制到工作区
-git clone git@github.com:liugang1104/ab_dsl_interpreter.git $WORKSPACE_DIR
+git clone git@github.com:liugang1104/ab_dsl_interpreter.git "$WORKSPACE_DIR/ab_dsl_interpreter"
 
 #3.dsl生成代码
-FLUTTER_DIR=`which flutter`
+FLUTTER_DIR=$(which flutter)
 FLUTTER_BIN="${FLUTTER_DIR%/*}"
-DARTSDK="${FLUTTER_BIN}/cache/dart-sdk/bin/dart"
-$DARTSDK "$WORKSPACE_DIR/ab_dsl_interpreter/ab_dsl_interpreter.dart"
+DART_SDK="${FLUTTER_BIN}/cache/dart-sdk/bin/dart"
+$DART_SDK pub get -C "$WORKSPACE_DIR/ab_dsl_interpreter"
+$DART_SDK "$WORKSPACE_DIR/ab_dsl_interpreter/lib/ab_dsl_interpreter.dart"
