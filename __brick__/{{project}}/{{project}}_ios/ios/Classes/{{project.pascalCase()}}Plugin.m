2b755227eaea4baebe50fcf274095c9c91ec29ff
mark.liu <mark.liu@ambergroup.io>
2022-08-03 17:33:38 +0800

    update

--- __brick__/{{project}}/{{project}}_ios/ios/Classes/{{project.pascalCase()}}Plugin.m
+++ __brick__/{{project}}/{{project}}_ios/ios/Classes/{{project.pascalCase()}}Plugin.m
@@ -1,18 +1,10 @@
 #import "{{project.pascalCase()}}Plugin.h"
-#if __has_include(<{{project}}_ios/{{project}}_ios-Swift.h>)
-#import <{{project}}_ios/{{project}}_ios-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "{{project}}_ios-Swift.h"
-#endif
 
 @implementation {{project.pascalCase()}}Plugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-    FlutterMethodChannel *channel = [FlutterMethodChannel methodChannelWithName:@"my_test_plugin"
+    FlutterMethodChannel *channel = [FlutterMethodChannel methodChannelWithName:@"io.ambergroup.plugins/{{project}}"
                                                                 binaryMessenger:[registrar messenger]];
-    {{project.pascalCase()}}Plugin* instance = [[{{project.pascalCase()}} alloc] init];
+    {{project.pascalCase()}}Plugin* instance = [[{{project.pascalCase()}}Plugin alloc] init];
     [registrar addMethodCallDelegate:instance channel:channel];
 }
 
