ef68b2f8aafd54c2a7de9d980448deb29b2a1d11
mark.liu <mark.liu@ambergroup.io>
2022-08-03 16:59:08 +0800

    update

--- __brick__/{{project}}/{{project}}/example/lib/main.dart
+++ __brick__/{{project}}/{{project}}/example/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:my_device/my_device.dart';
+import 'package:{{project}}/{{project}}.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
-  final _myDevicePlugin = MyDevice();
+  final _myPlugin = {{project.pascalCase()}}();
 
   @override
   void initState() {
@@ -32,7 +32,7 @@ class _MyAppState extends State<MyApp> {
     // We also handle the message potentially returning null.
     try {
       platformVersion =
-          await _myDevicePlugin.getPlatformVersion() ?? 'Unknown platform version';
+          await _myPlugin.getPlatformVersion() ?? 'Unknown platform version';
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
