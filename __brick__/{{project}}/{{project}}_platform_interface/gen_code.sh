f416e5d56f125f2f1cf284be9e0dcc9e14d3b2a2
I`m so vagetable <657769152@qq.com>
2022-08-10 15:21:38 +0800

    Update gen_code.sh

--- __brick__/{{project}}/{{project}}_platform_interface/gen_code.sh
+++ __brick__/{{project}}/{{project}}_platform_interface/gen_code.sh
@@ -3,15 +3,11 @@
 #1.新建DSL工作区
 cd ../
 PLUGIN_DIR=$(pwd)
-echo "$PLUGIN_DIR"
-
 PLUGIN_NAME=$(basename $(pwd))
-echo "$PLUGIN_NAME"
-
 WORKSPACE_DIR="$PLUGIN_DIR/workspace"
 mkdir "$WORKSPACE_DIR"
 
-#2.将dsl复制到工作区
+#2.clone dsl package
 git clone git@github.com:liugang1104/ab_dsl_interpreter.git "$WORKSPACE_DIR/ab_dsl_interpreter"
 
 #3.dsl生成代码
